bd8127745603f9ca8010b7b59df42276d9adf0da
Rodolfo Silva <100287751+Jrodolfosilva@users.noreply.github.com>
2023-06-16 12:00:56 -0300

    feat: creating page list condominio

---
+++ src/app/condominio/page.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const ContainerCondominio = styled.section`
+
+
+
+`
+
+
+
+export default function Condominio (){
+
+
+
+
+    return(
+        <ContainerCondominio>
+            <h1>Seção para listagem dos imóveis em Condomínios</h1>
+        </ContainerCondominio>
+    )
+}
